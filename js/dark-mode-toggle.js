50408d93ca4e056522ddae61d7698cb003024f94
dialectic-lab <chandnideadwyler@gmail.com>
2025-03-17 16:21:23 -0400

    typography

--- js/dark-mode-toggle.js
+++
@@ -1,9 +0,0 @@
-// Simple dark mode toggle
-document.addEventListener("DOMContentLoaded", function() {
-  const toggle = document.getElementById('dark-mode-toggle');
-
-  toggle.addEventListener('click', function() {
-    document.body.classList.toggle('theme-base-0d');
-    document.body.classList.toggle('theme-base-08');
-  });
-});
